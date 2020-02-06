534359c2a10c715db7b863fe092979bd80b4930c
yehorbk <e.bublyk1010@gmail.com>
2020-02-06 21:26:17 +0200

    demo updated

--- demo.js
+++ demo.js
@@ -1,5 +1,14 @@
 'use strict';
 
 const ArgumentsParser = require('./cli-argv-parser');
-const result = ArgumentsParser.parse(process.argv, { k: String, w: Number, tr: Boolean, q: String, x: String});
+
+const scheme = {
+    file: String,
+    force: Boolean,
+    count: Number,
+}
+
+const args = ['node', 'demo.js', '--file', 'app.js', 'force', '--count', '20'];
+
+const result = ArgumentsParser.parse(args, scheme);
 console.log(result);
