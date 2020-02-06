8a9ba53076538c7f21ec73964cc68a16059ebdb0
yehorbk <e.bublyk1010@gmail.com>
2020-02-06 20:51:54 +0200

    scheme functionality implemented

--- demo.js
+++ demo.js
@@ -1,5 +1,5 @@
 'use strict';
 
 const ArgumentsParser = require('./cli-argv-parser');
-const result = ArgumentsParser.parse(process.argv);
+const result = ArgumentsParser.parse(process.argv, { k: String, w: Number, tr: Boolean, q: String});
 console.log(result);
