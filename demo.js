1da03424e6bcc003fee826d2e5e1468308b0e2c8
yehorbk <e.bublyk1010@gmail.com>
2020-02-06 22:49:18 +0200

    eslint configured

--- demo.js
+++ demo.js
@@ -3,12 +3,14 @@
 const ArgumentsParser = require('./cli-argv-parser');
 
 const scheme = {
-    file: String,
-    force: Boolean,
-    count: Number,
-}
+  file: String,
+  force: Boolean,
+  count: Number,
+};
 
 const args = ['node', 'demo.js', '--file', 'app.js', 'force', '--count', '20'];
 
-const result = ArgumentsParser.parse(args, scheme); // If you want to try with cli arguments - change args to process.argv
+// If you want to try with cli arguments - change 'args' to 'process.argv'
+const result = ArgumentsParser.parse(args, scheme);
+
 console.log(result);
--- tests/parser.test.js
+++ tests/parser.test.js
@@ -3,29 +3,29 @@
 const ArgumentsParser = require('../lib/parser');
 
 const withoutScheme = {
-    correctResult: { '--key': 'k', '-w': '1000', 'root': true, '--q': 'val', '-x': '3' },
-    args: ['data', 'data', '--key', 'k', '-w', '1000', 'root', '--q', 'val', '-x', '3'],
-}
+  correctResult: { '--key': 'k', '-w': '1000', 'root': true, '--q': 'val', '-x': '3' },
+  args: ['data', 'data', '--key', 'k', '-w', '1000', 'root', '--q', 'val', '-x', '3'],
+};
 
 const withScheme = {
-    correctResult: { key: 'k', w: 1000, root: true, 'q': 'val', x: 3 },
-    args: ['data', 'data', '--key', 'k', '-w', '1000', 'root', '--q', 'val', '-x', '3'],
-    scheme: { key: String, w: Number, root: Boolean, q: String, x: Number },
-}
+  correctResult: { key: 'k', w: 1000, root: true, q: 'val', x: 3 },
+  args: ['data', 'data', '--key', 'k', '-w', '1000', 'root', '--q', 'val', '-x', '3'],
+  scheme: { key: String, w: Number, root: Boolean, q: String, x: Number },
+};
 
 const validate = (test, result) => {
-    const { correctResult } = test;
-    console.log(correctResult);
-    console.log(result);
-    for (const key in result) {
-        if (!correctResult[key]) {
-            throw new Error(`There is no to be ${key}`);
-        }
-        if (correctResult[key] !== result[key]) {
-            throw new Error(`The values are different: ${correctResult[key]} !== ${result[key]}`)
-        }
+  const { correctResult } = test;
+  console.log(correctResult);
+  console.log(result);
+  for (const key in result) {
+    if (!correctResult[key]) {
+      throw new Error(`There is no to be ${key}`);
     }
-}
+    if (correctResult[key] !== result[key]) {
+      throw new Error(`The values are different: ${correctResult[key]} !== ${result[key]}`);
+    }
+  }
+};
 
 validate(withoutScheme, ArgumentsParser.parse(withoutScheme.args));
 validate(withScheme, ArgumentsParser.parse(withScheme.args, withScheme.scheme));
